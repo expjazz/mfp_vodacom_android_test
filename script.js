bba714e1f571fc475fbea5ab4311c8f4bf8e21ab
Expedito Andrade <expeditojazz@gmail.com>
2022-07-30 14:09:20 -0300

    added POC

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ script.addEventListener('load', () => {
 })
 script.src = 'https://appx/web-view.min.js'
 document.getElementsByTagName('head')[0].appendChild(script);
+
+const getScript = () => {
+  alert('axios get script called');
+  fetch('https://appx/web-view.min.js', {
+    method: 'GET',
+  }).then(resp => {
+    alert('axios get script done' + JSON.stringify(resp.data));
+  }).catch(e => {
+    alert('axios get script error: ' + e);
+    alert('axios get script error: ' + JSON.stringify(e));
+    alert('axios get script error: ' + JSON.stringify(e.response));
+  });
+};
+
+setTimeout(() => {
+  getScript();
+}, 10000)
