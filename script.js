15d0ce24fcc73943be0daa527071dbbd801b03d6
Expedito Andrade <expeditojazz@gmail.com>
2022-07-30 14:54:25 -0300

    fix script loading

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-const script = document.createElement('script')
-script.type = 'text/javascript'
-script.addEventListener('load', () => {
-  alert('loaded script');
-  window.my = true;
-})
-script.src = 'https://appx/web-view.min.js'
-document.getElementsByTagName('head')[0].appendChild(script);
+// const script = document.createElement('script')
+// script.type = 'text/javascript'
+// script.addEventListener('load', () => {
+//   alert('loaded script');
+//   window.my = true;
+// })
+// script.src = 'https://appx/web-view.min.js'
+// document.getElementsByTagName('head')[0].appendChild(script);
 
 const getScript = () => {
   alert('function to get the script');
